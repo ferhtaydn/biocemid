e889a802855c972c9d633cbe934fbcf0b510ca6b
Ferhat Aydin <ferhtaydn@gmail.com>
2016-06-07 13:37:53 +0300

    INO feature is added.
    a few scores are gathered to check whether its usefullness.
    complete runs will be taken at overall evaluation step later.

--- src/main/scala/com/ferhtaydn/biocemid/annotators/tfrf/TfrfAnnotatorConfig.scala
+++ src/main/scala/com/ferhtaydn/biocemid/annotators/tfrf/TfrfAnnotatorConfig.scala
@@ -3,4 +3,5 @@ package com.ferhtaydn.biocemid.annotators.tfrf
 import com.ferhtaydn.biocemid.annotators.AnnotatorConfig
 
 final case class TfrfAnnotatorConfig(beforeAfterCount: Int, mainThreshold: Double, smallThreshold: Double,
-  outputFileSuffix: String, useNamedEntity: Boolean = false, pureBaseline: Boolean = false) extends AnnotatorConfig
+  outputFileSuffix: String, useNamedEntity: Boolean = false,
+  pureBaseline: Boolean = false, useINO: Boolean = false) extends AnnotatorConfig
